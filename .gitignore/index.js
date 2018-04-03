aaff389b9de704e234488325cbffb925737c7e1c
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2018-04-03 17:39:43 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,6 +13,6 @@ bot.login("NDMwMzQ4Mjk0NzgzMjM4MTQ0.DaRbaQ.F0RKMfMMs3wFg69Pvwg1iGRDgaU");
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("'''Liste des commandes disponibles : \n \n Le bot est actuellement en cours de développement.'''");
+        message.channel.sendMessage("```Liste des commandes disponibles : \n \n Le bot est actuellement en cours de développement.```");
     }
 });
