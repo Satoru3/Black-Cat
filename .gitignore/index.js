2c04e5a50510383d65c9d3fa3f2327f4cac1396d
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2018-04-03 21:09:10 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -16,3 +16,40 @@ bot.on('message', message => {
         message.channel.sendMessage("```Liste des commandes disponibles : \n \n *8ball \n Le bot est actuellement en cours de développement.```");
     }
 });
+
+bot.on("message", function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if (!message.content.startsWith(prefix)) return;
+
+    var args = message.content.substring(prefix.length).split(" ");
+
+    switch (args[0].toLowerCase()) {
+        case "roll":
+            var roll = Math.floor(Math.random() * args[1]) +1;
+            if (!roll) return message.reply("Entre un numéro =)")
+            message.channel.send("Je choisis le numéro " + roll + " !");
+            break;
+        case "8ball":
+        let args = message.content.split(" ").slice(1);
+        let tte = args.join(" ")
+        if (!tte){
+            return message.reply("Merci de me poser une question ! :P :8ball:")};
+
+            var replys = [
+                "Oui",
+                "Non",
+                "Je ne sais pas",
+                "Peut-être",
+                "Jamais !",
+                "Avec plaisir !"
+            ];
+
+            let reponse = replys[Math.floor(Math.random() * replys.length)])
+            var bembed = new Discord.RichEmbed()
+            .setDescription(":8ball: 8ball")
+            .addField("Question :", tte)
+            .addField("Réponse :", reponse)
+        message.channel.sendEmbed(bembed)
+        
+}})
