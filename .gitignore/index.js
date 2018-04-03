32de8c11d559a77c9e97f34d52872d6afac0fae7
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2018-04-03 17:44:07 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("*")
 
 bot.on('ready', function() {
-    bot.user.setGame("Command: *help");
+    bot.user.setGame("à *help");
     console.log("Connecté");
 });
 
