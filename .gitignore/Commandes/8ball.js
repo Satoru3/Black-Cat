f4d54a54f8638cb7b958b350ccd364b2d6a19ed6
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2018-04-03 22:34:13 +0200

    Add files via upload

---
+++ .gitignore/Commandes/8ball.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    if(!args[2]) return message.reply("Tu veux me poser une question ? =) :8ball:");
+    let replies = ["Oui.", "Non.", "Je ne sais pas.", "Je n'en suis pas sûr...", "Redemande moi plus tard !", "Exactement !"];
+
+    let result = Math.floor((Math.random() * replies.length));
+    let question = args.slice(1).join(" ");
+
+    let ballembed = new Discord.RichEmbed()
+    .setDescription(":8ball: 8ball")
+    .setColor("#FF9900")
+    .addField("Question :", question)
+    .addField("Réponse :", replies[result])
+    .setAuthor(message.author.tag);
+
+    message.channel.send(ballembed);
