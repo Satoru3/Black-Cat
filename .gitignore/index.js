9fbd8d65776f4dfc74a9dc4ecbe9f15caf00072f
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2018-04-03 02:34:26 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("*")
+
+bot.on('ready', function() {
+    bot.user.setGame("Command: :help");
+    console.log("Connecté");
+});
+
+bot.login("NDMwMzQ4Mjk0NzgzMjM4MTQ0.DaRbaQ.F0RKMfMMs3wFg69Pvwg1iGRDgaU");
+
+
+bot.on('message', message => {
+    if (message.content --- prefix + "help"){
+        message.channel.sendMessage("Liste des commandes disponibles : \n Le bot est actuellement en cours de développement.");
+    }
+});
