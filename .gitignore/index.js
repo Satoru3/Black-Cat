5d4b52b9b98b66b84347c638f063e7b88ab849bf
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2018-04-13 13:44:20 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -54,7 +54,7 @@ bot.on("message", function(message) {
             .setDescription(":8ball: 8ball")
             .addField("Question :", tte)
             .addField("Réponse :", reponse)
-            .addAuthor
+            .addField("Proposé par :", author)
 
         message.channel.sendEmbed(bembed)
 
