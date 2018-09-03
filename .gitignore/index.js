63aeacfd640c3b6eecd0e59f9b77dbacebd9518f
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2018-09-03 21:01:56 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -23,18 +23,6 @@ bot.on('message', message => {
         message.channel.sendEmbed(embed);
     }
 	
-    if (message.content === prefix + "ruby"){
-        var embed = new Discord.RichEmbed()
-            .setTitle("Ruby")
-            .setDescription("Voici un petit résumé de Ruby, et de ses performances !")
-            .addField("https://image.prntscr.com/image/LFZXg-WhSVK_k7cbuSW7Mg.png","Nous sommes les meilleurs","Augmente l'ATQ des Alliés du groupe de 60% si 5 Alliés Vent sont présent dans le groupe.", true)
-            .addField("*8ball","Petite commande pour s'amuser à poser des questions à la boule ;)", true)
-            .setColor("0xf55b1b")
-            .setFooter("Bonne journée sur Knights Chronicle FR ! =)"
-            .setThumbnail("http://prntscr.com/kq6bzr")
-        message.channel.sendEmbed(embed);
-    }
-	
 
     if (message.content === "Salut"){
         message.reply("Bonjour à toi ! =)");
