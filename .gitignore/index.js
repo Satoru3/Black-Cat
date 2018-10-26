29323c0ed93d8fd89653b8942798ffab3434f714
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2018-10-26 04:31:40 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -29,7 +29,7 @@ bot.on('message', message => {
         var embed = new Discord.RichEmbed()
 	    .setTitle(":black_small_square: Combattante téméraire - Ruby :black_small_square: SSR :black_small_square: Attaquante Vent :black_small_square: Vitesse de  base 599 :black_small_square: Unité Gacha :black_small_square: CV. Hiyori Kono")
 	    .setDescription("Fille au sang chaud élevée par son père, le gérant d'un dojo dans une petite ville du Grand duché de Delcart. L'irritation que suscite chez elle l'injustice l'a poussée à se proclamer «médiatrice citoyenne». C'est vraiment super... mais attendez-vous tout de même à ce que ce soient ses poings plus que ses paroles qui règlent les différends ! Son père doit souvent rembourser les dégâts qu'elle cause, mais le fringant maître de dojo préfère tout prendre avec le sourire.")
-	    .setField(":speech_balloon:**Compétence de chef : Nous sommes les meilleurs !**")
+	    .setField(":speech_balloon:**Compétence de chef: Nous sommes les meilleurs !**", true)
             .addField("Augmente de 60% l'ATQ des alliés de vent si le groupe est constitué de 5 héros de vent.", true)
 	    .setImage("https://image.prntscr.com/image/9ymXlBV3QhGkc_Ek1whRFw.png")
 	    .addField("__Compétence 2__","Blablalblalbal", true)
