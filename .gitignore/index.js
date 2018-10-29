e91a7a562485421dbd7ebc6a00e904036bcf688a
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2018-10-29 02:59:29 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -58,10 +58,10 @@ bot.on('message', message => {
 	    .addField(":crossed_swords: **Compétence 2: Coup de pied rotatif !** :hourglass: **3 tours**","Inflige des dégâts équivalents à 200% de l'ATQ à 1 cible. 65% de chances de réduire le temps de recharge de la compétence n°3 de 1 tour.", true)
 	    .addField(":crossed_swords: **Compétence 3: Coup de pied volant !** :hourglass: **5 tours**","Inflige des dégâts équivalents à 300% de l'ATQ à 1 cible.", true)
             .addField(":comet: **Visuel de l'animation de la compétence 3 :**",":arrow_down:", true)
-            .setImage("http://image.mobirum.com/GAME/knightschronicle_en/2226/2018/07/04/1530668815954.gif")
+            .setImage("https://images-ext-2.discordapp.net/external/B7qkPUcjjnyo6MTZHktSP0Rxhkih3g4oWDCrzRyYM0g/http/image.mobirum.com/GAME/knightschronicle_en/2226/2018/07/04/1530668815954.gif")
             .setColor("RANDOM")
-	    .setFooter("Toutes les compétences sont basées sur le Niv.1. Les skills Niv.2 à 6 arriveront ultérieurement. Le Héros est 6*." + " " + "Page de l'Héroïne Ruby générée suite à une demande de"+" "+ message.author.username + "." + " " + "Fiche mise à jour le 28/10")
-	    .setThumbnail("https://cdn.discordapp.com/attachments/432232468465188874/505159626459840512/H6rbMsZcmISiAAAAAElFTkSuQmCC.png")
+	    .setFooter("Toutes les compétences sont basées sur le Niv.1. Les skills Niv.2 à 6 arriveront ultérieurement. Le Héros est 6*. " + "Page de l'Héroïne Morrigan générée suite à une demande de " + message.author.username + ". " + "Fiche mise à jour le 29/10")
+	    .setThumbnail("https://image.prntscr.com/image/2irSx8SfQKS7Od2c86xUgA.png")
 	message.channel.sendEmbed(embed);
 
 
