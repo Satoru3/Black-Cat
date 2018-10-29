f53c8771bfc953c8ef1453c73712a849bd550bc3
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2018-10-29 04:25:30 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -167,7 +167,7 @@ bot.on("message", function(message) {
             .setDescription(":8ball: 8ball")
             .addField("Question :", tte)
             .addField("Réponse :", reponse)
-            .setAuthor(message.author.username + message.author.displayAvatarURL)
+            .setAuthor(message.author.username + "https://cdn.discordapp.com/attachments/432232468465188874/506295453239869440/Screenshot_11.png")
             .setThumbnail(message.author.avatarURL)
 	    .setFooter("Demandé par "+" "+ message.author.username)
             
