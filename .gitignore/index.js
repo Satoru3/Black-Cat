5ef361dca22115e28f4363c5fdc2092f88481b14
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2020-03-14 19:36:54 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -46,7 +46,7 @@ client.on("message", async message => {
         if (args[0].toLowerCase() === "embed") {
             const embed = new RichEmbed()
                 .setDescription(args.slice(1).join(" "))
-                .setColor(roleColor === "#000000" ? "#ffffff" :  roleColorv)
+                .setColor(roleColor === "#000000" ? "#ffffff" :  roleColor)
                 .setTimestamp()
                 .setImage(client.user.displayAvatarURL)
                 .setAuthor(message.author.username, message.author.displayAvatarURL);
