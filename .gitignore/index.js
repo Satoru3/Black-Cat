f81aad701396d31051f7d2c05ac7c29d7fb62cb4
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2020-03-14 19:32:30 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,11 +1,61 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const { Client, RichEmbed } = require("discord.js");
+const { config } = require("dotenv");
 
-var prefix = ("*")
+const client = new Client({
+    disableEveryone: true
+})
 
-bot.on('ready', function() {
-    bot.user.setActivity("*help | V.2.0 |");
-    console.log("Le bot a été connecté avec succès.");
+client.on("ready", () => {
+    console.log(`${client.user.username} est désormais connecté.`);
+
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "*help | V2",
+            type: "WATCHING"
+        }
+    }); 
+})
+
+client.on("message", async message => {
+    const prefix = "*";
+
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd === "ping") {
+
+        const msg = await message.channel.send(`Calcul en cours...`);
+
+        msg.edit(`Poooong ! Arky a fini ses calculs !\n La latence est de ${Math.floor(msg.createdTimestap - message.createdTimestap)}ms\n La latence de l'API est: ${Math.round(client.ping)}ms`);
+    }
+
+    if (cmd === "say") {
+
+        if (message.deletable) message.delete();
+
+        if (args.length < 0) return message.reply(`Tu n'as rien à dire ?`).then(m => m.delete(5000));
+        
+        const roleColor = message.guild.me.highestRole.hexColor;
+
+
+        if (args[0].toLowerCase() === "embed") {
+            const embed = new RichEmbed()
+                .setDescription(args.slice(1).join(" "))
+                .setColor(roleColor === "#000000" ? "#ffffff" :  roleColorv)
+                .setTimestamp()
+                .setImage(client.user.displayAvatarURL)
+                .setAuthor(message.author.username, message.author.displayAvatarURL);
+
+            message.channel.send(embed);
+        } else {
+            message.channel.send(args.join(" "));
+        }
+    }
 });
 
-bot.login(process.env.TOKEN);
+client.login(process.env.TOKEN);
