a879c414404fa9d3f3976ef436d4cca8591af161
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2020-03-14 19:48:19 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,59 +1,52 @@
-const { Client, RichEmbed } = require("discord.js");
+const { Client, Collection } = require("discord.js");
 const { config } = require("dotenv");
+const fs = require("fs");
 
 const client = new Client({
     disableEveryone: true
-})
+});
+
+client.commands = new Collection();
+client.aliases = new Collection();
+
+client.categories = fs.readdirSync("./commands/");
+
+});
+
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
 
 client.on("ready", () => {
-    console.log(`${client.user.username} est désormais connecté.`);
+    console.log(`Hi, ${client.user.username} is now online!`);
 
     client.user.setPresence({
         status: "online",
         game: {
-            name: "*help | V2",
-            type: "WATCHING"
+            name: "me getting developed",
+            type: "STREAMING"
         }
     }); 
-})
+});
 
 client.on("message", async message => {
-    const prefix = "*";
+    const prefix = "_";
 
     if (message.author.bot) return;
     if (!message.guild) return;
     if (!message.content.startsWith(prefix)) return;
+    if (!message.member) message.member = await message.guild.fetchMember(message);
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+    let command = client.commands.get(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
 
-    if (cmd === "ping") {
-
-        const msg = await message.channel.send(`Calcul en cours...`);
-
-        msg.edit(`Poooong ! Arky a fini ses calculs !\n La latence est de ${Math.floor(msg.createdTimestap - message.createdTimestap)}ms\n La latence de l'API est: ${Math.round(client.ping)}ms`);
-    }
-
-    if (cmd === "say") {
-
-        if (message.deletable) message.delete();
-
-        if (args.length < 0) return message.reply(`Tu n'as rien à dire ?`).then(m => m.delete(5000));
-        
-
-        if (args[0].toLowerCase() === "embed") {
-            const embed = new RichEmbed()
-                .setDescription(args.slice(1).join(" "))
-                .setColor("RANDOM")
-                .setTimestamp()
-                .setImage(client.user.displayAvatarURL)
-                .setAuthor(message.author.username, message.author.displayAvatarURL);
-
-            message.channel.send(embed);
-        } else {
-            message.channel.send(args.join(" "));
-        }
-    }
+    if (command) 
+        command.run(client, message, args);
 });
 
 client.login(process.env.TOKEN);
