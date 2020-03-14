ff52283f29416b69230e827dcafb254bdc69b12a
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2020-03-14 19:39:17 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -40,13 +40,11 @@ client.on("message", async message => {
 
         if (args.length < 0) return message.reply(`Tu n'as rien à dire ?`).then(m => m.delete(5000));
         
-        const roleColor = message.guild.me.highestRole.hexColor;
-
 
         if (args[0].toLowerCase() === "embed") {
             const embed = new RichEmbed()
                 .setDescription(args.slice(1).join(" "))
-                .setColor(roleColor === "#000000" ? "#ffffff" :  roleColor)
+                .setColor("RANDOM")
                 .setTimestamp()
                 .setImage(client.user.displayAvatarURL)
                 .setAuthor(message.author.username, message.author.displayAvatarURL);
