7dc3c1cf7a277f1590e2710bfbe6de3f31f8bc87
Satoru3 <38013927+Satoru3@users.noreply.github.com>
2020-03-14 22:36:44 +0100

    Update clear.js

--- .gitignore/commands/moderation/clear.js
+++ .gitignore/commands/moderation/clear.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "clear",
     aliases: ["purge", "nuke"],
     category: "moderation",
-    description: "Clears the chat",
+    description: "Arky fais le ménage.",
     run: async (client, message, args) => {
         if (message.deletable) {
             message.delete();
@@ -10,17 +10,17 @@ module.exports = {
     
         // Member doesn't have permissions
         if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-            return message.reply("You can't delete messages....").then(m => m.delete(5000));
+            return message.reply("Arky est obligé de te l'avouer, tu n'as pas la permission pour faire ceci !").then(m => m.delete(5000));
         }
 
         // Check if args[0] is a number
         if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
-            return message.reply("Yeah.... That's not a numer? I also can't delete 0 messages by the way.").then(m => m.delete(5000));
+            return message.reply("Tu sais, il faut un nombre, oh, et je ne suis pas capable de supprimer 0 message, idiot.").then(m => m.delete(5000));
         }
 
         // Maybe the bot can't delete messages
         if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
-            return message.reply("Sorryy... I can't delete messages.").then(m => m.delete(5000));
+            return message.reply("...Je ne peux pas supprimer ces messages... Désolé.").then(m => m.delete(5000));
         }
 
         let deleteAmount;
@@ -32,7 +32,7 @@ module.exports = {
         }
 
         message.channel.bulkDelete(deleteAmount, true)
-            .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages.`))
-            .catch(err => message.reply(`Something went wrong... ${err}`));
+            .then(deleted => message.channel.send(`Kawerick a détruit \`${deleted.size}\` messages.`))
+            .catch(err => message.reply(`Arky s'excuse... une erreur s'est produite... ${err}`));
     }
 }
